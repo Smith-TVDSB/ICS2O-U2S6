c1efe2be8a2762549e88ef373e2361b86b44c8d5
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-02-13 14:06:04 -0500

    test fixed

--- student_test.py
+++ student_test.py
@@ -1,50 +1,24 @@
 import pytest
 import student 
 
-def test_one():
+def test_one(capsys):
     student.cowabungaFunction(1)
     out, err = capsys.readouterr()
-    assert out.lower() == "cowabunga"
+    assert "cowabunga\n" == out.lower()
 
-def test_two():
+def test_two(capsys):
     student.cowabungaFunction(2)
     out, err = capsys.readouterr()
-    assert out.lower() == "cowabungacowabunga"
+    assert out.lower() == "cowabungacowabunga\n"
 
-def test_eight():
+def test_eight(capsys):
     student.cowabungaFunction(8)
     out, err = capsys.readouterr()
-    assert out.lower() == "cowabungacowabungacowabungacowabungacowabungacowabungacowabungacowabunga"
+    assert out.lower() == "cowabungacowabungacowabungacowabungacowabungacowabungacowabungacowabunga\n"
 
+def test_many(capsys):
+    for i in range(2,100):
+        student.cowabungaFunction(i)
+        out, err = capsys.readouterr()
+        assert out.lower() == ("cowabunga"*i)+"\n", "Make sure it scales, it didn't work for "+ str(i)+" cowabungas"
 
-
-
-#Standard test output ONLY (no input)
-#No need for the if __name__ condition in the main code, but might as well when done so students get used to it.
-"""def test_hello(capsys):
-    import hello
-    out,err = capsys.readouterr()
-    assert out == "Hello world!\n" or "bye" in out, "Should read 'Hello world!' "
-"""
-
-# Tests multiple values one at a time:
-
-# def test_higher():
-#     for i in [19, 91, 20, 24, 54, 33, 18]:
-#         input_values=[str(i)]
-#         output=[]
-
-#         def mock_input(s=None):
-#             if s is not None:
-#                 output.append(s)
-#                 return input_values.pop(0)
-#             else:
-#                 output.append("")
-#                 return input_values.pop(0)
-        
-#         student.input = mock_input
-#         student.print = lambda s : output.append(s)
-
-#         student.main()
-
-#         assert "true" in output[1].lower()
