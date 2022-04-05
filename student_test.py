994368b9bde7ee7ebe3fefbe81e7aac2c9799e03
Smith-TVDSB <70407532+Smith-TVDSB@users.noreply.github.com>
2022-04-05 08:55:10 -0400

    Update student_test.py

--- student_test.py
+++ student_test.py
@@ -4,21 +4,20 @@ import student
 def test_one(capsys):
     student.cowabungaFunction(1)
     out, err = capsys.readouterr()
-    assert "cowabunga\n" == out.lower(), "make sure you're using the function properly not just the main"
+    assert "cowabunga\n" == out.lower() or "cowabunga"==out.lower(), "make sure you're using the function properly not just the main"
 
 def test_two(capsys):
     student.cowabungaFunction(2)
     out, err = capsys.readouterr()
-    assert out.lower() == "cowabungacowabunga\n"
+    assert out.lower() == "cowabungacowabunga\n" or "cowabungacowabunga"==out.lower()
 
 def test_eight(capsys):
     student.cowabungaFunction(8)
     out, err = capsys.readouterr()
-    assert out.lower() == "cowabungacowabungacowabungacowabungacowabungacowabungacowabungacowabunga\n"
+    assert out.lower() == "cowabungacowabungacowabungacowabungacowabungacowabungacowabungacowabunga\n" or "cowabungacowabungacowabungacowabungacowabungacowabungacowabungacowabunga"==out.lower()
 
 def test_many(capsys):
     for i in range(2,100):
         student.cowabungaFunction(i)
         out, err = capsys.readouterr()
-        assert out.lower() == ("cowabunga"*i)+"\n", "Didn't work for"+str(i)
-
+        assert out.lower() == ("cowabunga"*i)+"\n" or  ("cowabunga"*i)==out.lower(), "Didn't work for"+str(i)
